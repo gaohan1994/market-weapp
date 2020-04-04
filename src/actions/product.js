b0779f56b6e8d8445829eb1ddcb87da17252e36a
gaohan1994 <871418277@qq.com>
2020-04-04 15:34:26 +0800

    * 修改

--- src/actions/product.js
+++ src/actions/product.js
@@ -166,6 +166,11 @@ class ProductAction {
     return result;
   }
 
+  fetchItemLike = async (params) => {
+    const result = await requestHttp.post(`/like`, params);
+    return result;
+  } 
+
   fetchProductCollect = async (params) => {
     const result = await requestHttp.get(`/collect/product${jsonToQueryString(params)}`);
     return result;
@@ -298,10 +303,16 @@ class ProductAction {
 
     const userinfo = loginManager.getUserinfo();
     let collect = {};
+    let like = {};
     if (userinfo.success) {
       const collectResult = await this.fetchProductCollect({user_id: userinfo.result.user_id, product_id: params.id});
       if (collectResult.code === ResponseCode.success) {
         collect = collectResult.data
+      }
+
+      const likeResult = await this.fetchItemLike({user_id: userinfo.result.user_id, item_id: params.id, type: 0});
+      if (likeResult.code === ResponseCode.success) {
+        like = likeResult.data
       }
     }
     if (result.code === ResponseCode.success) {
@@ -309,7 +320,8 @@ class ProductAction {
         type: constants.RECEIVE_PRODUCT_DETAIL,
         payload: {
           ...result.data,
-          collect
+          collect,
+          like
         }
       });
     }
